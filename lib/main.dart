559ada5802a9f3379d2130edc3529f985f6aa1b9
tenpei-sano <kikichan.fps@gmail.com>
2023-09-23 23:38:44 +0900

    firebase セットアップ

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:beta/presentation/page/home_page/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
